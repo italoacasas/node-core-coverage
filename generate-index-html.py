f327737035827f63f94e8b1a4e6d745a79b02a2c
Italo A. Casas <me@italoacasas.com>
2016-06-10 09:19:43 -0400

    fixing problems in mobile

--- generate-index-html.py
+++ generate-index-html.py
@@ -10,7 +10,6 @@ with open('out/index.html', 'w') as out:
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Node.js Core Coverage</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
